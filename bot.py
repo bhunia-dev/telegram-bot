2a8f9692cb40a6c2363d534bb748340c1b734f7a
Subhadip Bhunia <145113958+bhunia-dev@users.noreply.github.com>
2026-07-30 21:32:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
-from telegram.ext import ApplicationBuilder, MessageHandler, filters
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-# Your Telegram Bot API Token
-TOKEN = "8827531750:AAH_0sxgVeMag0X_VMY_ySaykkMoGihD2Kw"
+TOKEN = os.getenv("8827531750:AAH_0sxgVeMag0X_VMY_ySaykkMoGihD2Kw")
 
-async def handle(update, context):
-    user_text = update.message.text
-    await update.message.reply_text(f"You said: {user_text}")
+async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    await update.message.reply_text(f"You said: {text}")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.TEXT, handle))
